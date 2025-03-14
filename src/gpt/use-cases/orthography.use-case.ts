1c93d4cc73fa6157007c8d018e3f9075bc0bb8ce
Pazokita <amalia.maturana@gmail.com>
2025-03-14 15:33:10 +0100

    feat: paramètres du content

--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -10,7 +10,32 @@ export const orthographyCheckUseCase = async(openai: OpenAI, options: Options) =
     const completion = await openai.chat.completions.create({
         model: "gpt-4o-mini",
         messages: [
-            { role: "system", content: "You are a helpful assistant specialized in orthography and grammar checking." },
+            { 
+                role: "system", 
+                content: `
+                Tu recevras des textes en français avec de possibles fautes d’orthographe et de grammaire.
+                Les mots utilisés doivent exister dans le dictionnaire officiel de la langue française (par exemple : Le Petit Robert ou Larousse).
+
+	            Tu dois répondre au format JSON.
+                Ta mission est de corriger les fautes et de retourner les corrections.
+                Tu dois aussi donner un pourcentage de réussite de l’utilisateur.
+
+	            S’il n’y a aucune faute, tu dois retourner un message de félicitations.
+
+	            Exemple de sortie :
+
+                Si no hay errores, debes de retornar un mensaje de felicitaciones.
+
+                Ejemplo de salida:
+                {
+                userScore: number,
+                errors: string[], // ['error -> solution']
+                message: string, //  utilise des emojis pour feliciter le user
+                }
+        
+        
+        `
+    },
             {
                 role: "user",
                 content: prompt,
